32142918a7eb628d34b004ebaee924ab79434da7
John Nowinsky <armthepit@armthepit.com>
2016-10-10 14:41:54 -0500

    add switch to run functions for the different liri commands.

--- liri.js
+++ liri.js
@@ -1,5 +1,28 @@
+// Requires
+
 var fs = require('fs');
 var request = require('request');
 var spotify = require('spotify');
 var twitter = require('twitter');
 
+// Get Liri command
+
+liriCommand = process.argv[2];
+console.log(liriCommand);
+
+switch(liriCommand) {
+	case "my-tweets":
+	console.log("my-tweets");
+		break;
+	case "spotify-this-song":
+		console.log("spotify");
+		break;
+	case "movie-this":
+		console.log("movie");
+		break;
+	case "do-what-it-says":
+		console.log("do what");
+		break;
+	default:
+		console.log("no choice");
+}
