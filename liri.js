c83afffd4c55c1cea5e8e8c306657bb4cb133af0
John Nowinsky <armthepit@armthepit.com>
2016-10-11 07:44:21 -0500

    Added twitter function comments and console log for response answers.

--- liri.js
+++ liri.js
@@ -15,7 +15,7 @@ switch (liriCommand) {
         myTweets();
         break;
     case "spotify-this-song":
-        console.log("spotify");
+    	console.log("spotify");
         break;
     case "movie-this":
         console.log("movie");
@@ -28,9 +28,9 @@ switch (liriCommand) {
 }
 
 function myTweets() {
-
+	// Read in the key file.
     var keys = require('./keys.js');
-
+    // Read in the keyList and an assign the keys to the Twitter client
     var keyList = keys.twitterKeys;
 
     var client = new twitter({
@@ -39,12 +39,21 @@ function myTweets() {
         access_token_key: keyList.access_token_key,
         access_token_secret: keyList.access_token_secret
     });
-
-    var params = { screen_name: 'john_nowinsky', count: 20 };
+    // Assign the screen name and number of tweets to return.
+    // var params = { screen_name: 'john_nowinsky', count: 20 };
+    var params = { count: 7 };
+    // Get the tweets
     client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
-
+    	// Display the tweets and show error if there is an error
         if (!error) {
-            console.log(tweets);
+        	for(var i = 0; i < 7; i++) {
+            	console.log(tweets[i].created_at); 
+            	console.log(tweets[i].text);
+            	console.log('-----------')    
+            }      
+        } else {
+        	console.log(error);
         }
     });
 }
+
